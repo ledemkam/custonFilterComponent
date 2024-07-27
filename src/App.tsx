87a8d3a74fc80ab45eba8c41a6064fcc1041b104
ledemkam <81742990+ledemkam@users.noreply.github.com>
2024-07-27 22:30:17 +0200

    "all filters"

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetchProducts } from "./api/ProductsFilters";
+import { fetchProducts, ProductFilters } from "./api/ProductsFilters";
 import ProductList from "./components/product/ProductList";
 import FilterProductList from "./components/product/FilterProductList";
+import { useState } from "react";
 
 export default function App() {
+  const [search, setSearch] = useState<ProductFilters["search"]>();
+  const [category, setCategory] = useState<ProductFilters["category"]>();
+  const [maxPrice, setMaxPrice] = useState<ProductFilters["maxPrice"]>();
   const { data, isFetching } = useQuery({
-    queryKey: ["products"],
-    queryFn: () => fetchProducts(),
+    queryKey: ["products", { search , category, maxPrice }],
+    queryFn: () => fetchProducts({ search, category, maxPrice }),
   });
   return (
     <div className="flex flex-col gap-2">
-      <div>
-        <h1 className="text-4xl font-bold">Products</h1>
-      </div>
-      <FilterProductList />
-      <div>
-        {data && <ProductList products={data} />}
-        {isFetching && <p>Loading...</p>}
-      </div>
+        <div>
+          <h1 className="text-4xl font-bold">Products</h1>
+        </div>
+        <FilterProductList
+         onChange={(filters) => {
+            setSearch(filters.search)
+            setCategory(filters.category)
+            setMaxPrice(filters.maxPrice)
+          }}
+
+         
+         
+         />
+        <div>
+          {data && <ProductList products={data} />}
+          {isFetching && <p>Loading...</p>}
+        </div>
     </div>
   );
 }
--- src/components/product/FilterProductList.tsx
+++ src/components/product/FilterProductList.tsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ProductFilters } from "../../api/ProductsFilters";
+import { useDebounce } from "../../hooks/useDebounce";
 
 type FilterProductListProps = {
-  onFilter: (filters: ProductFilters) => void;
+  onChange: (filters: ProductFilters) => void;
 }
 
-export default function FilterProductList({ onFilter }: FilterProductListProps) {
+export default function FilterProductList({
+   onChange
+   }: FilterProductListProps) {
   const [search, setSearch] = useState<ProductFilters['search']>();
+  const [category, setCategory] = useState<ProductFilters['category']>();
+  const [maxPrice, setMaxPrice] = useState<ProductFilters['maxPrice']>();
+  const debouncedSearch = useDebounce(search);
+
+  useEffect(() => {
+    onChange({ search: debouncedSearch, category, maxPrice });
+  }, [debouncedSearch]);
 
 
   return (
@@ -16,7 +26,25 @@ export default function FilterProductList({ onFilter }: FilterProductListProps) 
        value={search}
        onChange={(e)=> setSearch(e.target.value)}
         placeholder="Search products..."
-             />  
+             />
+      <select
+         value={category}
+          onChange={(e) => setCategory(e.target.value as ProductFilters['category'])}       
+         >
+        <option value="first">First</option>
+        <option value="second">Second</option>
+        <option value="third">Third</option>
+      </select>
+      <select
+        value={maxPrice}
+        onChange={(e) =>
+          setMaxPrice(e.target.value ? parseInt(e.target.value) : undefined)
+        }
+      >
+        <option value="100">100</option>
+        <option value="500">500</option>
+        <option value="1000">1000</option>
+      </select>  
     </>
   )
 }
